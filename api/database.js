ad1685c71e5e8f40accac9e16b37c1b78dc2a05c
dbcaveira69 <douglasboleiro2015@gmail.com>
2026-02-22 19:52:20 -0300

    Update database.js

--- api/database.js
+++ api/database.js
@@ -6,12 +6,20 @@ export default async function handler(req, res) {
         try {
             const response = await fetch(GOOGLE_URL);
             const data = await response.json();
-            res.status(200).json(data);
-        } catch (e) { res.status(500).json({ error: 'Falha na leitura' }); }
+            return res.status(200).json(data);
+        } catch (e) { 
+            return res.status(500).json({ error: 'Falha na leitura' }); 
+        }
     } else if (req.method === 'POST') {
         try {
-            await fetch(GOOGLE_URL, { method: 'POST', body: JSON.stringify(req.body) });
-            res.status(200).json({ success: true });
-        } catch (e) { res.status(500).json({ error: 'Falha na escrita' }); }
+            await fetch(GOOGLE_URL, { 
+                method: 'POST', 
+                headers: { 'Content-Type': 'text/plain;charset=utf-8' }, // <- ISSO DESTRAVA O GOOGLE
+                body: JSON.stringify(req.body) 
+            });
+            return res.status(200).json({ success: true });
+        } catch (e) { 
+            return res.status(500).json({ error: 'Falha na escrita' }); 
+        }
     }
 }
